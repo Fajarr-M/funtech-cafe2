7da16cd351adbe292f4071fa2673bdf33ea12971
Fajar M <habibimochfajar.ah@gmail.com>
2021-01-30 09:41:50 +0700

    menambahkan project baru

---
+++ script.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+
+// Hamburger Menu
+$(".closeMenu").click(function(){
+    $(".mainMenu").css("top","-100%");
+});
+$(".openMenu").click(function(){
+    $(".mainMenu").css({
+        "display" : "flex", "top" : "0"
+    });
+});
+
+$(".box-menu").hover(function(){
+    $(".box-menu").css("opacity",".5");
+},
+function(){
+    $(".box-menu").css("opacity","1");
+});
+// Paralax
+
+$(window).on("load",function(){
+    $(".capt").addClass("captMuncul");
+});
+
+
+$(window).scroll(function(){
+    var wScroll = $(this).scrollTop();
+
+    $(".text-open").css({
+        "transform" : "translate("+ wScroll/25 +"%, 0px)"
+    });
+
+    $(".open img").css({
+        "transform" : "translate("+ wScroll/-25 +"%, 0px)"
+    });
+
+
+    // Navbar
+    if (wScroll > 5){
+        $("nav").addClass("bg");
+    }
+    else{
+        $("nav").removeClass("bg");
+    }
+
+    if (wScroll < 5){
+        $("nav ul li a").addClass("col");
+    }
+    else{
+        $("nav ul li a").removeClass("col");
+    }
+
+    // Material
+    if (wScroll > $(".container-mat").offset().top - 350 ){
+        $(".container-mat").addClass("muncul");
+    }
+
+    if (wScroll < $(".container-mat").offset().top - 600 ){
+        $(".container-mat").removeClass("muncul");
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
